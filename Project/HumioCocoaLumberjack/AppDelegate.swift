fc296c6a2569e80d10abb05b56aed84c05376adb
Jakob Jensen <jje@trifork.com>
2020-09-15 21:15:40 +0200

    Update example help.

--- Project/HumioCocoaLumberjack/AppDelegate.swift
+++ Project/HumioCocoaLumberjack/AppDelegate.swift
@@ -12,16 +12,16 @@ import CocoaLumberjack
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-//    private let logger = HumioLoggerFactory.createLogger("your token here", dataSpace:"some dataspace")
-    //or if you set up the info plist:
-    private let logger = HumioLoggerFactory.createLogger(loggerId: "a-star-found", verbose: true)
+    // Here are some ways to setup your logger:
+    // ----------------------------------------
+    // private let logger = HumioLoggerFactory.createLogger(accessToken: "your token here", dataSpace:"some dataspace")
+    // private let logger = HumioLoggerFactory.createLogger(accessToken: "your token here", dataSpace:"some dataspace", verbose: true) // with logging of humio logger, should not be enabled in production.
+    private let logger = HumioLoggerFactory.createLogger() // if you set it up using the info plist.
 
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        DDLog.add((DDTTYLogger.sharedInstance)) // or DDLog.add(DDOSLogger.sharedInstance) for iOS 10+
-
+        DDLog.add(DDOSLogger.sharedInstance)
         DDLog.add(logger, with: .error)
         DDLog.setLevel(.error, for: HumioCocoaLumberjackLogger.self)
-
         return true
     }
 
